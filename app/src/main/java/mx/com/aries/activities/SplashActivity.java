b74049a9dd252d89eaf23e23bfc417f03de4a96e
Meliton Tienda Avila <pin.meliton@gmail.com>
2017-09-14 21:41:22 -0500

    SplahScreen

---
+++ app/src/main/java/mx/com/aries/activities/SplashActivity.java
@@ -0,0 +1,24 @@
+package mx.com.aries.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.aries.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+
+       // Intent intent = new Intent(this, MainActivity.class);
+      //  startActivity(intent);
+       // finish();
+    }
+}
