664df64daf7b723e00e982c656ef92bec3e71a23
Meliton Tienda Avila <pin.meliton@gmail.com>
2017-09-14 21:56:43 -0500

    WelcomeActivity

---
+++ app/src/main/java/mx/com/aries/activities/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package mx.com.aries.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mx.com.aries.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
