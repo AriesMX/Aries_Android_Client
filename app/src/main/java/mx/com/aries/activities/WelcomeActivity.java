39755555d40523ce25062a4f24113b04caea8abc
Meliton Tienda Avila <pin.meliton@gmail.com>
2017-09-14 22:09:42 -0500

    IsFirtsTime

--- app/src/main/java/mx/com/aries/activities/WelcomeActivity.java
+++ app/src/main/java/mx/com/aries/activities/WelcomeActivity.java
@@ -1,16 +1,35 @@
 package mx.com.aries.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import mx.com.aries.R;
+import mx.com.aries.utils.SharedPreferenceUtil;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        start = (Button) findViewById(R.id.ac_welcome_start);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Intent intent = new Intent(context, MainActivity.class);
+                startActivity(intent);
+                SharedPreferenceUtil mSharedPreferences = SharedPreferenceUtil.getInstance(context);
+                mSharedPreferences.setValue("IsFirtsTime", false);
+                finish();
+            }
+        });
     }
 }
